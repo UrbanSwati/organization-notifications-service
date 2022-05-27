5b7efface7fb3e8e4c05381e24cd26482a1ca932
William Maphanga <williesdk@gmail.com>
2022-05-28 00:09:35 +0200

    updated config variables

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ from pydantic import BaseSettings, HttpUrl
 
 
 class Config(BaseSettings):
-    base_url: HttpUrl
-    recipient: str = 'hello@gmail.com'
+    base_api_url: HttpUrl
+    recipient: str = 'email@domain.com'
+    smtp_password: str
+    smtp_port: int = 587
+    smtp_sender_email: str
+    smtp_server: str
+    org_name: str
 
 
 config = Config(_env_file='.env')
